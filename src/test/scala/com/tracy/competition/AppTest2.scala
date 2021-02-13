d4674173925420709ea1043ff3aeb75db00062f1
Tracyyy-s <supertracyyy@126.com>
2021-02-13 20:16:39 +0800

    update test and yaml

--- src/test/scala/com/tracy/competition/AppTest2.scala
+++ src/test/scala/com/tracy/competition/AppTest2.scala
@@ -1,8 +1,10 @@
 package com.tracy.competition
 
+import java.lang.reflect.Field
 import java.util
 
 import com.tracy.competition.domain.entity.User
+import com.tracy.competition.service.UserService
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -17,10 +19,23 @@ import org.springframework.test.context.junit4.SpringRunner
 @RunWith(classOf[SpringRunner])
 class AppTest2 {
 
+  @Autowired val userService: UserService = null
 
   @Test
   def test01(): Unit = {
 
+    val user: User = userService.findUserByUserName("10000")
+
+    val clazz: Class[_ <: User] = user.getClass
+
+    val fields: Array[Field] = clazz.getDeclaredFields
+
+    fields.foreach(field => {
+      field.setAccessible(true)
+
+      val value: AnyRef = field.get(user)
+      println(field.getName + ": " + field.get(user))
+    })
   }
 
 }
